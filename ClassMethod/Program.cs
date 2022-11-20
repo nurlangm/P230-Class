8d25e0e7f476650b217db6764c33fcdee464e3e2
unknown <you@example.com>
2022-11-20 17:48:03 +0400

    Task

---
+++ ClassMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿#region Her Sozun Bas Herfi Boyuk
+//namespace P230task
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str = "bU TaSkI meTodLa yazMalISIZ";
+//            string title = CapitalizeFirstLetter(str);
+//            Console.WriteLine(title);
+
+//            static string CapitalizeFirstLetter(string value)
+//            {
+//                value = value.ToLower();
+//                char[] array = value.ToCharArray();
+//                if (array.Length >= 1)
+//                {
+//                    if (char.IsLower(array[0]))
+//                    {
+//                        array[0] = char.ToUpper(array[0]);
+//                    }
+//                }
+//                for (int i = 1; i < array.Length; i++)
+//                {
+//                    if (array[i - 1] == ' ')
+//                    {
+//                        if (char.IsLower(array[i]))
+//                        {
+//                            array[i] = char.ToUpper(array[i]);
+//                        }
+//                    }
+//                }
+//                return new string(array);
+//            }
+//        }
+//    }
+//} 
+#endregion
+
+#region Student Class
+//using ClassMethod;
+
+//Student student = new Student("Nurlan", "Mammadli", "P230", 90, false);
+
+
+
+//student.StudentAllName();
+//student.StudentInfo();
+//student.Mezun();
+//student.Imtahan();
+
+
+
+#endregion
---
+++ ClassMethod/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public int Point;
+        public bool IsGraduated;
+        public Student(string name, string surname, string group, int point, bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            IsGraduated = isGraduated;
+        }
+        public void StudentAllName()
+        {
+            Console.WriteLine(Name+" "+Surname);
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine(Name+" "+Surname+" "+Group+" " +Point+" "+IsGraduated);
+        }
+
+        public void Mezun()
+        {
+            if (IsGraduated)
+            {
+                Console.WriteLine("Telebe Mezun Olub");
+            }
+            else
+            {
+                Console.WriteLine("Telebe Mezun Olmayib");
+            }
+        }
+
+
+        public void Imtahan()
+        {
+            if (Point>80)
+            {
+                Console.WriteLine("Ikinci imtahana girmeye sansiniz var");
+            }
+            else
+            {
+                Console.WriteLine("Ikinci imtahana girmeye sansiniz yoxdur");
+            }
+        }
+    }
+}
